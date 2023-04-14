474343d17452e99547550bbe657852b6a1b0ebc6
matthias <git@melwert.de>
2023-04-14 20:24:30 +0200

    add figure sprites and refactoring

--- scenes/figure/figure.gd
+++ scenes/figure/figure.gd
@@ -2,30 +2,39 @@ extends CharacterBody2D
 
 class_name Figure
 
-const UP_PLAYER_ONE: String = "up_player_one"
-const UP_PLAYER_TWO: String = "up_player_two"
-const DOWN_PLAYER_ONE: String = "down_player_one"
-const DOWN_PLAYER_TWO: String = "down_player_two"
+const UP_PLAYER_LEFT: String = "up_player_left"
+const UP_PLAYER_RIGHT: String = "up_player_right"
+const DOWN_PLAYER_LEFT: String = "down_player_left"
+const DOWN_PLAYER_RIGHT: String = "down_player_right"
 
 @export var _movement_speed = 500.0
-@export var images: Array[ImageTexture]
+@export var images: Array[CompressedTexture2D]
 
-var _player = 1 # 1 = Player One, 2 = Player Two
+var _player = PLAYER_LEFT
 var input_direction: float = 0.0
 
+enum {
+	PLAYER_LEFT,
+	PLAYER_RIGHT
+}
 
 func _ready():
-	if _player == 1:
+	if _player == PLAYER_LEFT:
 		$Sprite2D.texture = images[0]
-	else:
+		$Sprite2D.rotation_degrees = 180
+	elif _player == PLAYER_RIGHT:
 		$Sprite2D.texture = images[1]
+	else:
+		printerr("Player type unrecognised: %s" % str(_player))
 
 
 func _physics_process(_delta):
-	if _player == 1:
-		input_direction = Input.get_axis(UP_PLAYER_ONE, DOWN_PLAYER_ONE)
+	if _player == PLAYER_LEFT:
+		input_direction = Input.get_axis(UP_PLAYER_LEFT, DOWN_PLAYER_LEFT)
+	elif _player == PLAYER_RIGHT:
+		input_direction = Input.get_axis(UP_PLAYER_RIGHT, DOWN_PLAYER_RIGHT)
 	else:
-		input_direction = Input.get_axis(UP_PLAYER_TWO, DOWN_PLAYER_TWO)
+		printerr("Player type unrecognised: %s" % str(_player))
 	
 	velocity = transform.y * input_direction * _movement_speed
 	
@@ -33,4 +42,7 @@ func _physics_process(_delta):
 
 
 func set_player(player: int) -> void:
-	_player = player
+	if player == PLAYER_LEFT or player == PLAYER_RIGHT:
+		_player = player
+	else:
+		printerr("Invalid player type: %s" % str(_player))
