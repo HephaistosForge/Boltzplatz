56e8ca61cac30971bcc175b8821bcbeb88d6011e
Robert <robert-github@posteo.de>
2023-04-14 20:10:19 +0200

    Update Figure

--- scenes/figure/figure.gd
+++ scenes/figure/figure.gd
@@ -1,28 +1,36 @@
 extends CharacterBody2D
 
+class_name Figure
+
 const UP_PLAYER_ONE: String = "up_player_one"
 const UP_PLAYER_TWO: String = "up_player_two"
 const DOWN_PLAYER_ONE: String = "down_player_one"
 const DOWN_PLAYER_TWO: String = "down_player_two"
-const PLAYER_ONE: int = 1
-const PLAYER_TWO: int = 2
 
-@export var movement_speed = 500.0
-@export var player = PLAYER_ONE # 1 = player one, 2 = player two
+@export var _movement_speed = 500.0
+@export var images: Array[ImageTexture]
 
+var _player = 1 # 1 = Player One, 2 = Player Two
 var input_direction: float = 0.0
 
 
 func _ready():
-	pass
+	if _player == 1:
+		$Sprite2D.texture = images[0]
+	else:
+		$Sprite2D.texture = images[1]
 
 
 func _physics_process(_delta):
-	if player == PLAYER_ONE:
+	if _player == 1:
 		input_direction = Input.get_axis(UP_PLAYER_ONE, DOWN_PLAYER_ONE)
 	else:
 		input_direction = Input.get_axis(UP_PLAYER_TWO, DOWN_PLAYER_TWO)
 	
-	velocity = transform.y * input_direction * movement_speed
+	velocity = transform.y * input_direction * _movement_speed
 	
 	move_and_slide()
+
+
+func set_player(player: int) -> void:
+	_player = player
