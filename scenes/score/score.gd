52dae8a742cadfb69e2f8c8c46089fbaf77dacbf
matthias <git@melwert.de>
2023-04-14 19:11:53 +0200

    add score interface

---
+++ scenes/score/score.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@onready var label_player_left = $HBoxContainer/LabelPlayerLeft
+@onready var label_player_right = $HBoxContainer/LabelPlayerRight
+
+var score_player_left = 0
+var score_player_right = 0
+
+
+func add_one_to_player_left():
+	score_player_left += 1
+	_update_ui()
+	
+func add_one_to_player_right():
+	score_player_right += 1
+	_update_ui()
+
+func reset_score():
+	score_player_left = 0
+	score_player_right = 0
+	_update_ui()
+	
+func _update_ui():
+	label_player_left.text = score_player_left
+	label_player_right.text = score_player_right
