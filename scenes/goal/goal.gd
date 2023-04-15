e93d38baa58b9c516747b73657b9c9b14c7a3807
Robert <robert-github@posteo.de>
2023-04-15 11:20:12 +0200

    Add goal scoring sfx
    - Adjust ball kick sfx pitch

--- scenes/goal/goal.gd
+++ scenes/goal/goal.gd
@@ -4,6 +4,7 @@ extends Area2D
 
 @onready var score = get_parent().get_parent().get_node("Score")
 @onready var ball = get_parent().get_parent().get_node("Ball")
+@onready var goal_scoring_sfx = preload("res://assets/audio/sfx/GoalScoring.mp3")
 
 enum GOAL_POSITION {
 	LEFT,
@@ -16,5 +17,10 @@ func _on_body_entered(_body):
 		score.add_one_to_player_right()
 	elif goal_position == GOAL_POSITION.RIGHT:
 		score.add_one_to_player_left()
-		
+	
+	var sfx = AudioStreamPlayer2D.new()
+	sfx.stream = goal_scoring_sfx
+	add_child(sfx)
+	sfx.play()
+	
 	ball.set_to_position(get_viewport().get_visible_rect().get_center())
--- scenes/rods_with_figures/sfx.gd
+++ scenes/rods_with_figures/sfx.gd
@@ -2,5 +2,5 @@ extends AudioStreamPlayer2D
 
 
 func play_with_random_pitch() -> void:
-	self.pitch_scale = randf_range(0.9, 1.4) # Fine tuned for slight sound variation
+	self.pitch_scale = randf_range(0.95, 1.25) # Fine tuned for slight sound variation
 	self.play()
