4270100d02a07141b987debd7cc55a340f44176a
Lui Dni <elisabeth-buettner@outlook.com>
2023-04-15 23:16:28 +0200

    adding popup for winscene

---
+++ scenes/score/popup.gd
@@ -0,0 +1,4 @@
+extends PanelContainer
+
+func _on_menu_pressed():
+	get_tree().change_scene_to_file("res://scenes/main_menu/main_menu.tscn")
--- scenes/score/score.gd
+++ scenes/score/score.gd
@@ -3,6 +3,9 @@ extends Control
 @onready var label_player_left = $HBoxContainer/LabelPlayerLeft
 @onready var label_player_right = $HBoxContainer/LabelPlayerRight
 
+const WIN_DIALOG_PREFAB = preload("res://scenes/score/popup.tscn")
+const POINTS_TO_WIN = 1
+
 var score_player_left = 0
 var score_player_right = 0
 
@@ -12,6 +15,7 @@ func _ready():
 func add_one_to_player_left():
 	score_player_left += 1
 	_update_ui()
+	check_score()
 	
 func add_one_to_player_right():
 	score_player_right += 1
@@ -25,3 +29,8 @@ func reset_score():
 func _update_ui():
 	label_player_left.text = str(score_player_left)
 	label_player_right.text = str(score_player_right)
+
+func check_score():
+		if score_player_left == POINTS_TO_WIN or score_player_right == POINTS_TO_WIN:
+			var win_dialog = WIN_DIALOG_PREFAB.instantiate()
+			self.add_child(win_dialog)
